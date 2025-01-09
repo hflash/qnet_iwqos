5a90228c23ee211b7cffc5326c8d8bd0e94a5d67
hflash <wonderloveu@gmail.com>
2025-01-09 18:49:25 +0800

    映射信息数据获取

--- qnet_iwqos/src/matrix2matrix.py
+++ qnet_iwqos/src/matrix2matrix.py
@@ -30,10 +30,41 @@ def map_nodes(graph_A, graph_B):
             a.append(abs(A_list[i]-B_list[j]))
         new_mat.append(a)
     row_ind, col_ind = linear_sum_assignment(new_mat)
-
-
     return row_ind, col_ind
 
+def cpmpute_mapping_information(graph_A, graph_B, row_ind, col_ind):
+    # adjacency_A = np.array(graph_A)
+    # adjacency_B = np.array(graph_B)
+
+    mapping_cost_information = {}
+    max_nodes = max(len(graph_A), len(graph_B))
+    padded_graph_A = pad_matrix(np.array(graph_A), (max_nodes, max_nodes))
+
+    A_list = cal_matrix(padded_graph_A)
+    B_list = cal_matrix(graph_B)
+    new_mat = []
+    for i in range(len(padded_graph_A)):
+        a=[]
+        for j in range(len(padded_graph_A)):
+            # if A_list[i] = 0
+            a.append(abs(A_list[i]-B_list[j]))
+        new_mat.append(a)
+    # row_ind, col_ind = linear_sum_assignment(new_mat)
+    cost = 0
+    if row_ind == None and col_ind == None:
+        for i  in range(len(new_mat)):
+            cost += new_mat[i][i]
+        mapping_cost_information['cost'] = cost
+    else:
+        for i, j in row_ind, col_ind:
+            cost += new_mat[i][j]
+    zero_count = 0
+    for row  in new_mat:
+        for num in row:
+            if num == 0:
+                zero_count += 1
+    mapping_cost_information['zero_num'] = zero_count
+    return mapping_cost_information
 
 
 # graph_A = [[0, 1, 0],
